a6f3a39e8ab3a2f2079cadfd75fd61b838b1a9bc
Victor Kozodaev <viktor7778123@gmail.com>
2021-01-30 15:46:34 +0300

    Init: Demo app

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Alice, Reply } = require('yandex-dialogs-sdk')
+const alice = new Alice();
+
+alice.command('', async ctx => Reply.text('Привет, это демо версия!'));
+
+alice.command('Запусти аквадискотеку', async ctx =>
+		Reply.text('Аквадискотеки сегодня не будет, устроишь ее когда купишь себе дворец'),
+);
+alice.any(async ctx => Reply.text(`Я не понимаю !!`));
+const PORT = 3001
+const server = alice.listen(PORT, '/');
+
