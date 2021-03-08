1f8790e6a7059fc0c0c0d7a2bbae9ce1aa12155d
Victor Kozodaev <viktor7778123@gmail.com>
2021-03-08 19:12:05 +0300

    Make MVP (feat: add tasks to ticktick via voice input)

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
-const { Alice, Reply } = require('yandex-dialogs-sdk')
-const alice = new Alice();
+const ticktick = require('ticktick-wrapper');
 
-alice.command('', async ctx => Reply.text('Привет, это демо версия!'));
-
-alice.command('Запусти аквадискотеку', async ctx =>
-		Reply.text('Аквадискотеки сегодня не будет, устроишь ее когда купишь себе дворец'),
-);
-alice.any(async ctx => Reply.text(`Я не понимаю !!`));
-
-const PORT = process.env.PORT || 3001
-const server = alice.listen(PORT, '/');
-
+module.exports.handler = async (event, _context) => {
+	
+	const {version, session, request, state} = event;
+	
+	const output = {
+		version,
+		session,
+		response: {
+			text: 'Привет, ты можешь добавить задачу!',
+			end_session: false,
+		},
+	};
+	
+	const login = async () => {
+		const authData = await ticktick.login({
+			email: {
+				username: process.env.TICKTICK_EMAIL,
+				password: process.env.TICKTICK_PASSWORD,
+			}
+		});
+		
+		output.user_state_update = authData
+	}
+	// Auth
+	if (state.user && state.user.cookie && state.user.userInfo) {
+		ticktick.restoreSession(state.user)
+	} else {
+		await login()
+	}
+	
+	const addTask = async () => {
+		if (request['original_utterance'].length > 0) {
+			const taskTitle = request['original_utterance']
+			
+			const description = 'Задача с Яндекс станции';
+			await ticktick.Inbox.addSimpleTask(taskTitle, description);
+			
+			output.response.text = `${taskTitle}, Успешно добавлена!`
+			output.response.end_session = true
+		}
+	}
+	
+	try {
+		await addTask()
+	} catch {
+		await login()
+		await addTask()
+	}
+	
+	return output;
+};
